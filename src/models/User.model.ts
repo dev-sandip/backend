07d897a2e16db3b607ea62203640defc9037a39c
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-26 08:48:39 +0545

    Initial Commit

---
+++ src/models/User.model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+/**
+ * Represents the schema for a user in the application.
+ * @remarks This schema contains the data of the user that is publicly available to other users
+ */
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 40,
+  },
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 40,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profileImg: {
+    type: String,
+    default: "https://res.cloudinary.com/dxxxkeonq/image/upload/v1705631570/website/xgolsvu8xejzsqj8jjku.jpg",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  isOnline: {
+    type: Boolean,
+    default: false,
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  password: {
+    type: String,
+    min: 8,
+    max: 20,
+  },
+
+});
+
+export default mongoose.model("User", UserSchema);
